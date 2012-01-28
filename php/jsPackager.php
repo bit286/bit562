51c0bbe3e82841b9f791518f8d95b792f0daa98f
cmgriffing <cmgriffing@gmail.com>
2012-01-27 16:26:46 -0800

    branchin out

---
+++ php/jsPackager.php
@@ -0,0 +1,35 @@
+<?php
+
+// phpPackager puts HTML around php class name lines, comments, method calls,
+// and codelines.
+class jsPackager implements Packager {
+   
+   private $divCount = 0;
+   
+   function __construct() {
+      
+   }
+   
+   public function packager($fileLine, $braceCount) {
+      
+      $html = '';
+      
+      if (strpos($fileLine, 'function') > -1) {
+         $this->divCount = $braceCount;
+      }
+      
+      // Code for putting HTML around the code line goes here.
+      
+      // Close off the nested <div> surrounding the code.
+      if ($divCount === $braceCount) {
+         $html .= '</div>';
+      }
+      
+      return $html;
+   }
+   
+   public function fnStripper()
+   {
+		
+   }
+}

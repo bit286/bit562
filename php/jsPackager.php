d2d35fc392d7db93550e8dd271bb0b634492e02f
Tim Finnell <tim.finnell@gmail.com>
2012-01-28 23:56:55 -0800

    test creates new jsPackager object, created matchers for line comments and functions

--- php/jsPackager.php
+++ php/jsPackager.php
@@ -4,32 +4,41 @@
 // and codelines.
 class jsPackager implements Packager {
    
-   private $divCount = 0;
+	private $divCount = 0;
    
-   function __construct() {
+	function __construct() {
       
-   }
+	}
    
-   public function packager($fileLine, $braceCount) {
+	public function packager($fileLine, $braceCount) {
       
-      $html = '';
-      
-      if (strpos($fileLine, 'function') > -1) {
-         $this->divCount = $braceCount;
-      }
-      
-      // Code for putting HTML around the code line goes here.
-      
-      // Close off the nested <div> surrounding the code.
-      if ($divCount === $braceCount) {
-         $html .= '</div>';
-      }
-      
-      return $html;
-   }
-   
-   public function fnStripper()
-   {
+		$html = '';
+		$commentExpr = '/^.*(\/\/.*$)+/';
+		$functionExpr = '/^.*(function)+(.\w)*\(.*\)( )*{$/';
+		$codelineExpr = '//';
+
+		if (strpos($fileLine, 'function') > -1) {
+			$this->divCount = $braceCount;
+		}
 		
-   }
+		// Code for putting HTML around the code line goes here.
+
+		// matches a line comment
+		$commentMatch = preg_match($commentExpr, $fileLine, $lineComment);
+		if ($commentMatch === 1) {
+			return "<span class=\"comment\">$lineComment[1]</span>";
+		}
+
+		$functionMatch = preg_match($functionExpr, $fileLine, $lineFunction);
+		if ($functionMatch === 1) {
+			return "<span class=\"function\">$lineFunction[0]</span>";
+		}
+			
+		// Close off the nested <div> surrounding the code.
+		if ($this->divCount === $braceCount) {
+			$html .= '</div>';
+		}
+		
+		return $html;
+	}
 }

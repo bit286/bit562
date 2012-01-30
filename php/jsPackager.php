7894d5b3d5537d9fae259b366993e93075125a52
cmgriffing <cmgriffing@gmail.com>
2012-01-30 07:13:52 -0800

    fixed my tabs to spaces

--- php/jsPackager.php
+++ php/jsPackager.php
@@ -4,110 +4,109 @@
 // and codelines.
 class jsPackager implements Packager {
    
-	private $divCount = 0;
-	
-   
-	function __construct() {
-      
-	}
-   
-	public function packager($fileLine, $braceCount) {
-      
-		$html = '';
-		$commentExpr = '/^.*(\/\/.*$)+/';
-		$functionExpr = '/^.*(function)+(.\w)*\(.*\)( )*{$/';
-		$codelineExpr = '//';
-		
-		/*
-		if (strpos($fileLine, 'function') > -1) {
-			$this->divCount = $braceCount;
-		}
-		
-		// Code for putting HTML around the code line goes here.
-		
-		// matches a line comment
-		$commentMatch = preg_match($commentExpr, $fileLine, $lineComment);
-		if ($commentMatch === 1) {
-			return "<span class=\"comment\">$lineComment[1]</span>";
-		}
+    private $divCount = 0;
 
-		$functionMatch = preg_match($functionExpr, $fileLine, $lineFunction);
-		if ($functionMatch === 1) {
-			return "<span class=\"function\">$lineFunction[0]</span>";
-		}
-		
-		
-		
-		
-		// Close off the nested <div> surrounding the code.
-		if ($this->divCount === $braceCount) {
-			$html .= '</div>';
-		}
-		*/
-		
-		
-		$commented = $this->commentParse($fileLine);
-		$thenFunctioned = $this->functionParse($commented);
-		
-		if ($fileLine == $thenFunctioned)
-		{
-			$html = '<div class="code-line">' . $thenFunctioned . '</div>';
-			
-		}
-		else
-		{
-			$html = $thenFunctioned;
-		}
-		
-		
-		
-		
-		
-		return $html;
-	}
-	
-	public function commentParse($line)
-	{	
-		$myCommentRegex = "/(?<!\:)\/\//";
-		$matched = preg_match($myCommentRegex, $line, $theMatches);
-		if($matched === 1)
-		{
-			//explode($theMatches[1], $line, $theLineArray);
-			$startOfComment = strpos($line, $theMatches[0]) - 1;
-			if ($startOfComment > 0)
-			{
-				$theLineArray = str_split($line, $startOfComment);
-				$newLine = '';
-				$newLine .= $theLineArray[0];
-				$newLine .= '<div class="comment">';
-				$theCount = count($theLineArray);
-				for ($i = 1; $i < $theCount; $i++)
-				{
-					$newLine .= $theLineArray[$i];
-				}
-				$newLine .= '</div>';
-			}
-			else
-			{
-				$newLine = '<div class="comment">' . $line . '</div>';
-			}
-			
-			$line = $newLine;
-		}		
-		//return '<div class="code-line">' . $line . '</div>';
-		return $line;
-	}
-	
-	public function functionParse($line)
-	{
-		$myFunctionRegex = '/(function)(?!\(\))/';		
-		$matched = preg_match($myFunctionRegex, $line, $theMatches);
-		if ($matched === 1)
-		{
-			$line = '<div class="functionBlock">' . $line . '</div>';
-			$this->divCount += 1;
-		}
-		
-		return $line;
-	}
+    function __construct() {
+      
+    }
+   
+    public function packager($fileLine, $braceCount) {
+      
+        $html = '';
+        $commentExpr = '/^.*(\/\/.*$)+/';
+        $functionExpr = '/^.*(function)+(.\w)*\(.*\)( )*{$/';
+        $codelineExpr = '//';
+        
+        
+        if (strpos($fileLine, 'function') > -1) {
+            $this->divCount = $braceCount;
+        }
+        
+        // Code for putting HTML around the code line goes here.
+        
+        // matches a line comment
+        $commentMatch = preg_match($commentExpr, $fileLine, $lineComment);
+        if ($commentMatch === 1) {
+            return "<span class=\"comment\">$lineComment[1]</span>";
+        }
+
+        $functionMatch = preg_match($functionExpr, $fileLine, $lineFunction);
+        if ($functionMatch === 1) {
+            return "<span class=\"function\">$lineFunction[0]</span>";
+        }
+        
+        
+        
+        
+        // Close off the nested <div> surrounding the code.
+        if ($this->divCount === $braceCount) {
+            $html .= '</div>';
+        }
+        
+        
+        /*
+        $commented = $this->commentParse($fileLine);
+        $thenFunctioned = $this->functionParse($commented);
+        
+        if ($fileLine == $thenFunctioned)
+        {
+            $html = '<div class="code-line">' . $thenFunctioned . '</div>';
+            
+        }
+        else
+        {
+            $html = $thenFunctioned;
+        }
+        */
+        
+        
+        
+        
+        return $html;
+    }
+    
+    public function commentParse($line)
+    {    
+        $myCommentRegex = "/(?<!\:)\/\//";
+        $matched = preg_match($myCommentRegex, $line, $theMatches);
+        if($matched === 1)
+        {
+            //explode($theMatches[1], $line, $theLineArray);
+            $startOfComment = strpos($line, $theMatches[0]) - 1;
+            if ($startOfComment > 0)
+            {
+                $theLineArray = str_split($line, $startOfComment);
+                $newLine = '';
+                $newLine .= $theLineArray[0];
+                $newLine .= '<div class="comment">';
+                $theCount = count($theLineArray);
+                for ($i = 1; $i < $theCount; $i++)
+                {
+                    $newLine .= $theLineArray[$i];
+                }
+                $newLine .= '</div>';
+            }
+            else
+            {
+                $newLine = '<div class="comment">' . $line . '</div>';
+            }
+            
+            $line = $newLine;
+        }        
+        //return '<div class="code-line">' . $line . '</div>';
+        return $line;
+    }
+    
+    public function functionParse($line)
+    {
+        $myFunctionRegex = '/(function)(?!\(\))/';        
+        $matched = preg_match($myFunctionRegex, $line, $theMatches);
+        if ($matched === 1)
+        {
+            $line = '<div class="functionBlock">' . $line . '</div>';
+            $this->divCount += 1;
+        }
+        
+        return $line;
+    }
 }
--- php/tests/jsPackager_test.php
+++ php/tests/jsPackager_test.php
@@ -1,66 +1,67 @@
 <!DOCTYPE html>
 <html>
-	<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-			<title>fileReader.php Test</title>
-			<style>
-				.comment { color: red; }
-			</style>
-	</head>
-	<body>
-		<?php
-			require_once('../Packager.php');
-			require_once('../jsPackager.php');
+    <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>fileReader.php Test</title>
+            <style>
+                .comment { color: red; }
+            </style>
+    </head>
+    <body>
+        <?php
+            require_once('../Packager.php');
+            require_once('../jsPackager.php');
 
-			$jsPackager = new jsPackager();
-			
-			$lineComment1 = 'some code goes here // this is a line comment';
-			$lineComment2 = '// this is a line comment';
+            $jsPackager = new jsPackager();
+            
+            $lineComment1 = 'some code goes here // this is a line comment';
+            $lineComment2 = '// this is a line comment';
 
-			$lineFunction1 = 'var = function() {';
-			$lineFunction2 = 'function junkfunction(var1, var2) {';
+            $lineFunction1 = 'var = function() {';
+            $lineFunction2 = 'function junkfunction(var1, var2) {';
 
-			$line1 = $jsPackager->packager($lineComment1, 0);
-			$line2 = $jsPackager->packager($lineComment2, 0);
-			$line3 = $jsPackager->packager($lineFunction1, 0);
-			$line4 = $jsPackager->packager($lineFunction2, 0);
+            $line1 = $jsPackager->packager($lineComment1, 0);
+            $line2 = $jsPackager->packager($lineComment2, 0);
+            $line3 = $jsPackager->packager($lineFunction1, 0);
+            $line4 = $jsPackager->packager($lineFunction2, 0);
 
-			/*
-			echo "<p>comment 1 in:<br />$lineComment1</p>\n";
-			echo "<p>comment 1 out:<br />$line1</p>\n";
+            /*
+            echo "<p>comment 1 in:<br />$lineComment1</p>\n";
+            echo "<p>comment 1 out:<br />$line1</p>\n";
 
-			echo "<p>comment 2 in:<br />$lineComment2</p>\n";
-			echo "<p>comment 2 out:<br />$line2</p>\n";
+            echo "<p>comment 2 in:<br />$lineComment2</p>\n";
+            echo "<p>comment 2 out:<br />$line2</p>\n";
 
-			echo "<p>function 1 in:<br />$lineFunction1</p>\n";
-			echo "<p>function 1 out:<br />$line3</p>\n";
+            echo "<p>function 1 in:<br />$lineFunction1</p>\n";
+            echo "<p>function 1 out:<br />$line3</p>\n";
 
-			echo "<p>function 2 in:<br />$lineFunction2</p>\n";
-			echo "<p>function 2 out:<br />$line4</p>\n";*/
-			
-			echo $line1;
-			echo $line2;
-			echo $line3;
-			echo $line4;
-			
-			
-			
-			
-			
-			
-			/*
-			$fileName = 'test.js';
-			if (is_file($fileName))
-			{     
-				 $fileHandle = fopen($fileName, 'r');
-				 $braceCount = 0;
-				 // Go through the file line by line.
-				 while (!feof($fileHandle)) {
-					$firstLine = fgets($fileHandle);
-					echo $jsPackager->packager($firstLine, $braceCount); 
-				 }
-			 }
-			 */
-		?>
-	</body>
+            echo "<p>function 2 in:<br />$lineFunction2</p>\n";
+            echo "<p>function 2 out:<br />$line4</p>\n";*/
+            
+            /*
+            echo $line1;
+            echo $line2;
+            echo $line3;
+            echo $line4;
+            */
+            
+            
+            
+            
+            
+            
+            $fileName = 'test.js';
+            if (is_file($fileName))
+            {     
+                 $fileHandle = fopen($fileName, 'r');
+                 $braceCount = 0;
+                 // Go through the file line by line.
+                 while (!feof($fileHandle)) {
+                    $firstLine = fgets($fileHandle);
+                    echo $jsPackager->packager($firstLine, $braceCount); 
+                 }
+             }
+             
+        ?>
+    </body>
 </html>

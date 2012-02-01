ec172a9658232e14689cc54a870ca09d77ebb0b0
Tom Christilaw <t.christilaw@gmail.com>
2012-02-01 13:45:28 -0800

    Changes to parser and new test text file.

--- php/tests/planguageParserTest.php
+++ php/tests/planguageParserTest.php
@@ -7,69 +7,58 @@
 <body>
                
 <?php
-            $planguage = 'gibberish gibberish gibberish gibberish gibberish gibberish gibberish   /*+SAMPLE::language=SQL::name=Study1::usage=First Code Sample: Show how to use declare statements::illustrations=DECLARE, SET, PRINT;;
-                         TABLE::tablename=test::database=B122_dbdesign::description=testing out string removal function::filename=test::description=test stuff::filedescription=moretest::description=test more stuff       */   gibberish gibberish gibberish gibberish 
-                         gibberish gibberish gibberish gibberish gibberish gibberish gibberish gibberish  
-                         /*+TABLE::tablename=users::database=pctools::description=Description of the database
-                         */ gibberish gibberish gibberish gibberish gibberish';
-       
-    echo '<h3>This is the full text string to extract the comment from.</h3>';
-            
-    echo $planguage;  
+
    
     
-     $searchString = '/*+SAMPLE::language=SQL::name=Study1::usage=First Code Sample: Show how to use declare statements::illustrations=DECLARE, SET, PRINT;;';
+     $searchString = '/*+       TABLE::tablename=users::database=pctools::description=Description of the database';
  
      echo '<h3>This is the first planguage line that will be passed to us.  
-         We will use this to search for the start of the comment.</h3>';
+         We will use this to search for the start of the comment in the file.</h3>';
      
      echo $searchString;
      
-     $start = strpos ($planguage, $searchString);
+     
+     $file = fopen ('parserTesterFile.txt', r);
+     $fileString = '';
+         while (!feof($file)) {
+            $lineString = fgets ($file);
+            if ($lineString === false) continue;
+            $lineString = trim($lineString);
+            if (strlen($lineString) == 0) continue;
+            $fileString .= $lineString;
+        }
+        fclose($file);
+     
+     echo '<h3>Our file converted to a string, ready for planguage extraction.</h3>';
+     
+     echo $fileString;
+        
+     $start = strpos ($fileString, $searchString);
      $start = $start + 3;
-     $end = strpos ($planguage, '*/', $start);
-     $fullString = substr (substr ($planguage, 0, -(strlen($planguage) - $end)), $start);
-     $fullString = trim ($fullString);
+     $end = strpos ($fileString, '*/', $start);
+     $planguageString = substr (substr ($fileString, 0, -(strlen($fileString) - $end)), $start);
+     $planguageString = trim ($planguageString);
+     
+      echo $planguageString;
+
+
      
      echo '<h3>Our string after extracting the planguage block and cropping the comment markup and any leftover whitespace on the ends.</h3>';
      
-     echo $fullString;
+     echo $planguageString;
      
-     $i = strpos ($fullString, ';;');
      
-     if (is_int($i)) {
-         $commandPairs = array();
-         $commandSections = explode(';;', $fullString);
-         foreach ($commandSections as $a) {
-             $commandParts = explode('::', $a);
-             $commandParts = array_map('trim',$commandParts);
-             $commandName = $commandParts[0];
-             $commandPairs[] = $commandName;
-             foreach ($commandParts as $b) {
-                 list ($name,$value) = explode('=', $b);
-                 $commandPairs[$name] = $value;
-         }
-         }
- 
-          } else {
-              $commandPairs = array();
-              $commandParts = explode('::', $fullString);
-              $commandParts = array_map('trim',$commandParts);              
-              $commandName = $commandParts[0];
-              $commandPairs[] = $commandName;
-              foreach ($commandParts as $a) {
-                  list ($name,$value) = explode('=',$a);
-                  $commandPairs[$name] = $value;
-                  
-              }
-          }
+     
+
+         $commandSections = explode(';;', $planguageString);
+
 
         
           
      
      echo '<h3>print_r output of the array.</h3>';     
           
-     print_r ($commandPairs);     
+     print_r ($commandSections);     
      
 
         ?>

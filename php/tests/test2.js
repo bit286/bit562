2a5419b13cc4ef7a715d80a97a08bc11c2f44139
Tim Finnell <tim.finnell@gmail.com>
2012-02-01 21:52:36 -0800

    refactored jsPackager and test

--- php/tests/test2.js
+++ php/tests/test2.js
@@ -1,24 +1,27 @@
 //Comment case 1 is this line
 
-//Comment Case 2: same line comment
-var commentCase2 = new Object();//here is the test comment
+var commentCase2 = new Object(); //here is the test comment
 
-//Comment case 3: should we be parsing for multi-line comments?
-//unimplemented
+//Function Case 1: the word function in the comment
 
-//Func Case 1: the word function in the comment
-
-//Func Case 2
-function someFunc() {
+//Function Case 2
+function test1() {
     var funcCase2 = new Object();
 }
 
-//Func Case 3: func with comment on same line
-function anotherFunc() {//testing comment on same line as func declaration
-    var funcCase3 = new Object();
+//Function Case 4: var = func()
+var funcCase2 = function() {
+    var extraVar = new Object();
 }
 
-//Func Case 4: var = func() , should this be a func-block
-var funcCase4 = function() {
-    var extraVar = new Object();
+//multi-line block
+function test2() {
+		var variable1 = 1;
+		var variable2 = 2;
+		var variable3 = variable1 + variable2;
+		var funcCase3 = function() {
+				var extraVar = new Object();
+		}
+
+		alert(variable3);
 }

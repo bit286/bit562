ea30ee51bd5066bff9daa4140cb409ac814be3a0
Tim Finnell <tim.finnell@gmail.com>
2012-02-02 23:52:07 -0800

    added commentBlock toggle and related conditions, moved open div to be based on brace count to account for non-function blocks eg. condition blocks.

--- php/tests/jsPackager_test.php
+++ php/tests/jsPackager_test.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>fileReader.php Test</title>
-		<style>
-			.comment { color: red; }
-			div { border: 1px solid blue; margin: 5px; padding: 5px; }
-		</style>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>fileReader.php Test</title>
+        <style>
+            .comment { color: red; }
+            div { border: 1px solid blue; margin: 5px; padding: 5px; }
+        </style>
     </head>
     <body>
         <?php
             require_once('../Packager.php');
             require_once('../jsPackager.php');
 
-			$jsPackager = new jsPackager();
+            $jsPackager = new jsPackager();
 
-            $fileName = 'test2.js';
+            $fileName = 'test.js';
             if (is_file($fileName))
             {     
-				$braceCount = 0;
+                $braceCount = 0;
                 $fileHandle = fopen($fileName, 'r');
                 // Go through the file line by line.
                 while (!feof($fileHandle)) {
                 $line = fgets($fileHandle);
-				if (strpos($line, "{") > -1) {
-					$braceCount += 1;
-				}
-				if (strpos($line, "}") > -1) {
-					$braceCount -= 1;
-				}
-				$line =  $jsPackager->packager($line, $braceCount); 
-#				echo "<p>$braceCount</p>";
-				echo $line;
-			}
-			}
-		?>
+                if (strpos($line, "{") > -1) {
+                    $braceCount += 1;
+                }
+                if (strpos($line, "}") > -1) {
+                    $braceCount -= 1;
+                }
+                $line =  $jsPackager->packager($line, $braceCount); 
+#               echo "<p>$braceCount</p>";
+                echo $line;
+            }
+            }
+        ?>
     </body>
 </html>

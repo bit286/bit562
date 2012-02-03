dc3db58f2e65e6716bf48b5b532c25e6d6035be6
Tim Finnell <tim.finnell@gmail.com>
2012-02-02 22:11:49 -0800

    removed line start/end from function reg-expression

--- php/jsPackager.php
+++ php/jsPackager.php
@@ -12,36 +12,36 @@ class jsPackager implements Packager {
     public function packager($fileLine, $braceCount) {
         
         $html = '';
-        $cmExpr = '/(?<!\:)\/\/.*$/';
-        $fnExpr = '/^(.*)function(.\w)*\(.*\)( )*{$/';
-		$emExpr = '/^( )*$/';
+        $cmExpr = '/(?<!:)\/\/.*$/';
+        $fnExpr = '/(.*)function(.\w)*\(.*\)( )*{/';
+        $emExpr = '/^( )*$/';
         $codelineExpr = '';
 
-		switch ($fileLine) {
-			// matches comment
-			case (preg_match($cmExpr, $fileLine, $lnCm) ? TRUE : FALSE ) :
-				$codeLine = explode("//", $fileLine);
+        switch ($fileLine) {
+            // matches comment
+            case (preg_match($cmExpr, $fileLine, $lnCm) ? TRUE : FALSE ) :
+                $codeLine = explode("//", $fileLine);
                 isset($lnCm[0]) AND
                     $html = "$codeLine[0]<span class='comment'>$lnCm[0]</span><br />";
-				break;
-			// matches function
-			case (preg_match($fnExpr, $fileLine, $lnFn) ? TRUE : FALSE) :
+                break;
+            // matches function
+            case (preg_match($fnExpr, $fileLine, $lnFn) ? TRUE : FALSE) :
                 $this->divCount = $braceCount;
                 $html = "<div><div>$fileLine</div>";
-				break;
-			// matches empty line
-			case (preg_match($emExpr, $fileLine, $lnEm) ? TRUE : FALSE) :
-				break;
+                break;
+            // matches empty line
+            case (preg_match($emExpr, $fileLine, $lnEm) ? TRUE : FALSE) :
+                break;
 
-			default :
-				$html = "<div class='codeLine'>$fileLine</div>";
-		}
+            default :
+                $html = "<div class='codeLine'>$fileLine</div>";
+        }
         
         // Close off the nested <div> surrounding the code.
         if ($this->divCount > $braceCount) {
           $this->divCount = $braceCount;
           $html .= '</div>';
         }
-		return $html;
+        return $html;
     }
 }

7168e7b9ba8b2d5244da981c6ce2d8506c3d8347
cmgriffing <cmgriffing@gmail.com>
2012-02-05 00:33:51 -0800

    had wrong relative path for entry header

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>anothertest</title>
-        <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="css/style.css" />
         
     </head>
     <body>

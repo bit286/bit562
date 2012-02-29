f4fbd357981ef63c3135f2a2cb2d12c5c2d6da03
Tom Christilaw <t.christilaw@gmail.com>
2012-02-29 13:18:51 -0800

    small changes

--- sql/projectfileInserts.sql
+++ sql/projectfileInserts.sql
@@ -1,4 +1,4 @@
-﻿insert into PROJECTFILES (Object_ID,Project,Source,Destination,Name,Description,Category)
+insert into PROJECTFILES (Object_ID,Project,Source,Destination,Name,Description,Category)
 Values
 ( 'qwert-qwert-qwezz-qaswe', 'BIT561',
  'c://xampp/htdocs/BIT561/php/reader.php',

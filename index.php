0a7729e58b13ddb81603bf5c311a9396b830f5e6
melissa <melissa@melissa-laptop>
2012-03-09 10:21:49 -0800

    Added new menu options to index.php and made it the default index.php file.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <p align="center"><font face="wp_bogus_font"><br />
 </font></p>
 <ol>
-<li><a title="" href="php/jumpPointDoc.php" style="font-family: wp_bogus_font">RUN</a><span style="font-family: wp_bogus_font">&nbsp;jumpPointDoc.php : Processes All documents refferenced in the projectfiles db</span></li>
+<li><a title="" href="php/jumpPointDoc.php" style="font-family: wp_bogus_font">RUN</a><span style="font-family: wp_bogus_font">&nbsp;jumpPointDoc.php : Run all files in projectfiles db through html wrapping function and store the output in the doc folder.</span></li>
 <li><a title="" href="jsTest.php" style="font-family: wp_bogus_font">RUN</a><span style="font-family: wp_bogus_font">&nbsp;.JS file test</span></li>
 <li><a title="" href="cssTest.php" style="font-family: wp_bogus_font">RUN</a><span style="font-family: wp_bogus_font"> .CSS file test</span></li>
 <li><a title="" href="phpTest.php" style="font-family: wp_bogus_font">RUN</a><span style="font-family: wp_bogus_font"> .PHP file test</span></li>

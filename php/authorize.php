f72ae40f6741d78ec46dcf7ed58ff3a80f0709d0
Tom Christilaw <t.christilaw@gmail.com>
2012-03-10 14:57:27 -0800

    little formatting

--- php/authorize.php
+++ php/authorize.php
@@ -10,13 +10,13 @@ $newCheck = new CheckUser ($username, $password);
 $result = $newCheck->authenticate();
 
 if ($result == 1) {
-				session_start();
-			    $_SESSION['username'] = $username;
-			    $_SESSION['password'] = $password;
-			    $_SESSION['loggedIn'] = true;
-			    header("Location:../index.php");
-			} else {
-				echo 'Login Failed';
-			}
+		session_start();
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password;
+		$_SESSION['loggedIn'] = true;
+		header("Location:../index.php");
+} else {
+		echo 'Login Failed';
+}
 
 ?>

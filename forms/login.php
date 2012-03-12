2eb73ec8b1d9c5871211b05ca1e8597064c1e6c7
Tom Christilaw <t.christilaw@gmail.com>
2012-03-12 07:45:26 -0700

    replaced Forms

---
+++ forms/login.php
@@ -0,0 +1,55 @@
+<html>
+  <head>
+    <title>Login</title>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+
+    <link rel="stylesheet" href="../css/bit561.css" type="text/css" />
+  </head>
+  <body>
+
+    <div id="leftside">
+      <img src="../images/leftside.jpg" width="125" height="1275"></img>
+    </div>
+
+    <div id="rightside">
+      <img src="../images/rightside.jpg" width="125" height="1275"></img>
+    </div>
+
+    <div id="topside">
+      <h2 align="center" id="pagetitle">Login</h2>
+    </div>
+
+    <div id="helpComments">
+    </div>
+
+    <div id="bgframe">
+      <div id="instructions">
+      </div>
+
+      <div id="main">
+
+        <h3>BIT561 -- Cascadia Professional Studies</h3>
+
+        <hr color="black" width="670px" />
+
+        <table><tr><td width="150" valign="center"><font size="+1" style="bold">
+                <strong id="formTitle">Please Login</strong></font></td>
+          </tr>
+        </table><br />
+        <form method="POST" action="../php/authorize.php">
+      Username:<br />
+      <input type="text" name="userName" />
+      <br /><br />
+      Password:<br />
+      <input type="password" name="password" />
+      <br /><br />
+      <input type="submit" id="subbut" value="Submit" />
+    </form>
+        
+      </div>
+    </div>
+
+    
+  </body>
+</html>

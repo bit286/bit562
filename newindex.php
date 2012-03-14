b5d166a76f4efe36967a55aaf4890b81002138ab
melissa <melissa@melissa-laptop>
2012-03-14 02:34:43 -0700

    Added copy text to newindex.php and added login session code.

--- newindex.php
+++ newindex.php
@@ -1,10 +1,21 @@
+
+<?php
+session_start();
+
+if ($_SESSION['loggedIn'] != true)
+  header('location:forms/login.php');
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
 <head>
    
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>The Next Generation I.T. Club - TNGITC.COM</title>
-    <meta name="description" content="Class project for BIT 286 - BIT561- Professional Tools" />
+    <title>NexISgen.com - BIT561</title>
+    <meta name="description" content="Class project for BIT 286 - BIT561- Professional Tools. Mission
+
+Construct a database driven website which supports the development of professional knowledge in a variety of languages; supports the documentation and integration of the work of software project team members; and increases the long-term productivity of IT professionals.
+" />
     <meta name="keywords" content="the,next,generation,information,technology,club,cascadia,community,college,bothell,bit286,bit561,professional tools" />
 
 
@@ -38,7 +49,7 @@
                 <div class="art-headerobject"></div>
                 <div class="art-logo">
                                  <h1 class="art-logo-name"><a href="../newindex.php">BIT 285 Class Project</a></h1>
-                                                 <h2 class="art-logo-text">BIT561 - Professional Tools</h2>
+                                                 <h2 class="art-logo-text">NexISgen.Com - BIT561 - Professional Software Development Tools</h2>
                                 </div>
                 </div>
             </div>
@@ -51,8 +62,8 @@
 <div class="art-nav-wrapper">
 <div class="art-nav-inner">
 	<ul class="art-hmenu">
-		<li>
-			<a href="index.php" class="active">Home</a>
+		<li>   
+                 <a href="index.php" class="active">Home</a>
 		</li>
 <li><a href="#">Forms</a>
 <ul>	
@@ -100,8 +111,8 @@
         <h3 style="text-align: center;">Features</h3>
 <p>List of project featuers goes here</p>
     </div><div class="art-layout-cell layout-item-0" style="width: 34%;">
-        <h3 style="text-align: center;">Vision</h3>
-<p>Project vision info goes here</p>
+        <h3 style="text-align: center;">Mission</h3>
+<p>Construct a database driven website which supports the development of professional knowledge in a variety of languages; supports the documentation and integration of the work of software project team members; and increases the long-term productivity of IT professionals. </p>
     </div><div class="art-layout-cell layout-item-0" style="width: 33%;">
         <h3 style="text-align: center;">Benefits</h3>
 <p>Project benefits info goes here</p>
@@ -141,16 +152,45 @@
         
             <li>Robotics group builds bluetooth controled robot.</li>
         
-            <li>Android applications planed.</li>
+            <li>Android applications coming soon.</li>
         </ul>
         
         <p><a href="https://cascadia.collegiatelink.net/organization/The_Next_Generation_I_T_Club" class="art-button">Learn more</a></p>
     </div><div class="art-layout-cell layout-item-3" style="width: 67%;">
-        <h3 style="text-align: center;">Group Support</h3>
+        <h3 style="text-align: center;">Project Vision</h3>
+        <p>The path to profound knowledge and wisdom in a field can be elusive. In a book entitled “Talent is Overrated” Geoff Colvin sums up the research on the development of expertise as a two part proposition: (1) you have to put your 10,000 hours in, and (2) you have to engage in deliberate practice. Research in a number of fields has shown that simply putting in your time is not enough.  As Colvin puts it, “…many people not only failed to become outstandingly good at what they do, no matter how many years they spend doing it, they frequently don’t even get any better than they were when they started.”  Nonetheless, you have to put the time in.  Colvin cites considerable research which casts doubt on the idea of innate talent being much of a predictor of superior performance.</p>  
+<p>Along with the time on task (10,000 hours minimum) comes the idea of deliberate practice.  You must focus on what you don’t know. Again in Colvin’s words, “…deliberate practice requires that one identify certain sharply defined elements of performance that need to be improved, and then work intensely on them…. The great performers isolate remarkably specific aspect of what they do and focus on just those things until they are improved; then it’s on to the next aspect.” Deliberate practice, focusing on what you do not know, is fairly intense.  It takes working up to and even in high level performers four hours a day of deliberate practice is about all they can handle.</p>
+
+<p>In a cognitive field like information technology your ability to learn quickly is an essential skill.  It is a skill that improves as your knowledge base gets larger.  Our capacity to remember well is fairly specific to our field of expertise.  “Indeed, top performers’ deep understanding of their field becomes the structure on which they can hang the huge quantities of information they learn about it…. It’s clear that the superior memory of great performers doesn’t just happen.  Since it is built on deep understanding of the field, it can be achieved only through years of intensive study.” [Geoffrey Colvin]</p>
+
+<p>Professional expertise, then, comes with a price tag, the learning curve must be scaled with effort and with carefully structured practice. The IT field is no exception, but what is particularly odd about IT is that there is so little discussion of the tool set that could be created on a computer to help with the process. What would a tool that assists in the development of a professional skill set look like?</p>
+
+<p>Some suggestions for inclusion in ProTools</p>
+
+        <ol>
+            <li>A professional toolkit would presumably include tested and debugged code that could be reused.  For example, validation is a necessity for data in any application.  The code used to provide validation could be structured to make it easy to include in both the front end and the middle tier.  The code could also coordinate validation in the two places through the database.  Other examples would include code to handle data passage between the browser and the database.  Code that we create for ourselves needs to be located (where are we storing it) and documented.</li>
+            <li>    Tested and debugged code might not be stored in separate files.  Smaller pieces of reusable code (snippets) might be kept in a database.  The code might be directly drawn from the database for inclusion in applications dynamically.  HTML particularly lends itself to this use.  The snippets also might be simply stored for cut and paste inclusion into an application.</li>
+            <li>    Code and discussion might also be created simply for the purpose of studying a language.  An example might be using callbacks in PHP function parameter lists.  Some sample code on what it looks like and some discussion might be included just to help prevent forgetting how it works.</li>
+            <li>    Non code discussion of data processing information that you want to remember could also be included.  Discussions and recording of ideas from agile methodology might be an example.</li>
+            <li>    Information tracking coding standards that will be followed in one’s professional programming efforts might also be of interest.</li>
+            <li>    URL storage of web sites that have useful programming information would be valuable.  The problem with favorite lists on browsers is that they don’t follow us as we move about the web.  There are some good web sites for sharing URLs with other people on the Web, however, they are standalone applications that do not necessarily integrate well with other stored professional knowledge.</li>
+            <li>    Evaluations of available software tools could also prove useful.  The proliferation of high quality frameworks helping solve various application problems is hard to keep up with available solutions.</li>
+            <li>    Since documentation is a professional necessity, the site should support and encourage good documentation habits.  The tools should support the creation of automatically generated documentation in web site form.  In so far as possible documentation should be driven by information contained within the source code documents creating a software application.</li>
+            <li>    Integration of work is a major concern for IT professionals, and so the site should provide support for the integration of project work.  In particular the small tasks derived from the overall project vision and used to create product (overall) and sprint (short interval) backlogs should be published in the database.  Who has committed to work on small projects, time estimates, and actual completion information is valuable and needs to be tracked for each project a team or individual undertakes.  The tracking allows improvement and agile is focused on continuous improvement.</li>
+        </ol>
+            <p>Expecting every programmer to build a site for their own professional use is unrealistic.  It’s a problem that takes considerable effort to resolve well.  Individuals cannot and do not undertake it lightly.  It’s a common problem for IT people and consequently it’s a problem that should have one or a few generic tools available for long term professional development.  A group of IT knowledgeable folks should have better success at solving the problem than an individual would have.</p>
+
+            <p>A site supporting professional development would need to provide that support over an extended time period—careers last for 40 years and beyond.  </p>
+
+            <p>The site would be driven by open source licensed code. Any professional using it could have access to the code driving the site so as to permit modification and extension.  Presumably, many groups might want to create a clone of the site for their specific purposes and to gain control over the long term viability of their particular site.</p>
         
+        </p>
         <p style="text-align: center;"><img width="300" height="200" alt="" src="./images/clublogo.png" /></p>
         
-        <p style="text-align: center;font-weight: bold;"> <span style="color: rgb(35, 141, 222);"><a href="https://www.facebook.com/pages/The-Next-Generation-IT-Club-tngitccom/240155819383182?skip_nax_wizard=true">CLUB FACEBOOK PAGE</a></br>Meetings: Wednesday 11 am - 1 pm Room: CC3-234</br>Cascadia Community College, Bothell, WA</span></p>
+        <p style="text-align: center;font-weight: bold;"> <span style="color: rgb(35, 141, 222);"><a href="https://www.facebook.com/pages/The-Next-Generation-IT-Club-tngitccom/240155819383182?skip_nax_wizard=true">CLUB FACEBOOK PAGE</a></br>Meetings: Wednesday 11 am - 1 pm Room: CC3-234</br>Cascadia Community College, Bothell, WA</br>
+            
+            
+            </span></p>
     </div>
     </div>
 </div>

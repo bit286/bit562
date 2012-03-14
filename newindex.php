f445a2dcfb7cf0e057bb7e3f1e91c0a45e34215b
melissa <melissa@melissa-laptop>
2012-03-14 01:36:34 -0700

    Added new menu system

---
+++ newindex.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
+<head>
+   
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>The Next Generation I.T. Club - TNGITC.COM</title>
+    <meta name="description" content="Class project for BIT 286 - BIT561- Professional Tools" />
+    <meta name="keywords" content="the,next,generation,information,technology,club,cascadia,community,college,bothell,bit286,bit561,professional tools" />
+
+
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
+    <!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+   <style type="text/css">
+.art-post .layout-item-0 { color: #0A2D47; background:repeat #F5F5F5; }
+.art-post .layout-item-1 { margin-bottom: 20px; }
+.art-post .layout-item-2 { padding: 20px; }
+.art-post .layout-item-3 { color: #0A2D47; background:repeat #F5F5F5; padding: 20px; }
+   .ie7 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+   .ie6 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+   </style>
+
+</head>
+<body>
+<div id="art-page-background-glare-wrapper">
+    <div id="art-page-background-glare"></div>
+</div>
+<div id="art-main">
+    <div class="cleared reset-box"></div>
+    <div class="art-header">
+        <div class="art-header-position">
+            <div class="art-header-wrapper">
+                <div class="cleared reset-box"></div>
+                <div class="art-header-inner">
+                <div class="art-headerobject"></div>
+                <div class="art-logo">
+                                 <h1 class="art-logo-name"><a href="../newindex.php">BIT 285 Class Project</a></h1>
+                                                 <h2 class="art-logo-text">BIT561 - Professional Tools</h2>
+                                </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="cleared reset-box"></div>
+<div class="art-bar art-nav">
+<div class="art-nav-outer">
+<div class="art-nav-wrapper">
+<div class="art-nav-inner">
+	<ul class="art-hmenu">
+		<li>
+			<a href="index.php" class="active">Home</a>
+		</li>
+<li><a href="#">Forms</a>
+<ul>	
+		<li>
+                        <a href="../bit561/forms/projectfiles.php" class "active">Project Files</a>
+                </li>
+<li>
+<a href="../bit561/forms/users.php">Users</a>
+</li>
+</ul>
+			
+<li>
+<a href="../bit561/forms/login.php#">Login</a>
+
+		<ul>
+                
+                  
+                
+                  <li>
+                    <a href="https://github.com/tfinnell/bit561">GitHub Project</a>
+                  </li>
+                 
+
+	</ul>
+		</li>	
+	</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="cleared reset-box"></div>
+<div class="art-box art-sheet">
+        <div class="art-box-body art-sheet-body">
+            <div class="art-layout-wrapper">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-content">
+<div class="art-box art-post">
+    <div class="art-box-body art-post-body">
+<div class="art-post-inner art-article">
+                                <div class="art-postcontent">
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 33%;">
+        <h3 style="text-align: center;">Features</h3>
+<p>List of project featuers goes here</p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 34%;">
+        <h3 style="text-align: center;">Vision</h3>
+<p>Project vision info goes here</p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 33%;">
+        <h3 style="text-align: center;">Benefits</h3>
+<p>Project benefits info goes here</p>
+    </div>
+    </div>
+</div>
+<div class="art-content-layout-wrapper layout-item-1">
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell" style="width: 33%;">
+        <p><img width="195" height="195" alt="" src="./images/contentimage7.png" style="margin:0;" /></p>
+        
+        <p style="text-align: center;"><a href="http://www.freedigitalphotos.net/images/view_photog.php?photogid=721">Image: renjith krishnan / FreeDigitalPhotos.net</a></p>
+    </div><div class="art-layout-cell" style="width: 34%;">
+        <p style="text-align: center;"><img width="195" height="195" alt="" src="./images/contentimage6.png" style="margin:0;" /></p>
+        
+        <p style="text-align: center;"><a href="http://www.freedigitalphotos.net/images/view_photog.php?photogid=721">Image: renjith krishnan / FreeDigitalPhotos.net</a></p>
+    </div><div class="art-layout-cell" style="width: 33%;">
+        <p style="text-align: center;"><img width="195" height="195" alt="" src="./images/contentimage4.png" style="margin:0;" /></p>
+        
+        <p style="text-align: center;"><a href="http://www.freedigitalphotos.net">Image: FreeDigitalPhotos.net</a></p>
+    </div>
+    </div>
+</div>
+</div>
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-2" style="width: 33%;">
+        <h3 style="text-align: center;">Latest Updates</h3>
+        
+        <ul>
+            <li>Student hosting BETA site launched 1/22/2012.</li>
+        
+            <li>Game design group moves forward with B.I.T. Spring Technology Fair 2012.</li>
+        
+            <li>Web hosting managment internships in the works.</li>
+        
+            <li>Robotics group builds bluetooth controled robot.</li>
+        
+            <li>Android applications planed.</li>
+        </ul>
+        
+        <p><a href="https://cascadia.collegiatelink.net/organization/The_Next_Generation_I_T_Club" class="art-button">Learn more</a></p>
+    </div><div class="art-layout-cell layout-item-3" style="width: 67%;">
+        <h3 style="text-align: center;">Group Support</h3>
+        
+        <p style="text-align: center;"><img width="300" height="200" alt="" src="./images/clublogo.png" /></p>
+        
+        <p style="text-align: center;font-weight: bold;"> <span style="color: rgb(35, 141, 222);"><a href="https://www.facebook.com/pages/The-Next-Generation-IT-Club-tngitccom/240155819383182?skip_nax_wizard=true">CLUB FACEBOOK PAGE</a></br>Meetings: Wednesday 11 am - 1 pm Room: CC3-234</br>Cascadia Community College, Bothell, WA</span></p>
+    </div>
+    </div>
+</div>
+
+                </div>
+                <div class="cleared"></div>
+                </div>
+
+		<div class="cleared"></div>
+    </div>
+</div>
+
+                          <div class="cleared"></div>
+                        </div>
+                        <div class="art-layout-cell art-sidebar1">
+<div class="art-box art-block">
+    <div class="art-box-body art-block-body">
+                <div class="art-bar art-blockheader">
+                    <h3 class="t">Free Hosting Benefits</h3>
+                </div>
+                <div class="art-box art-blockcontent">
+                    <div class="art-box-body art-blockcontent-body">
+                <p><img width="53" height="53" alt="" src="./images/1.png" style="float: left; margin:10px;" /></p>
+
+<p><span style="font-weight: bold;">Free CPANEL LINUX Cloud Hosting</span></p>
+
+<p>100 MB of space for your project portfolio.</p><br />
+
+<p><img width="53" height="53" alt="" src="./images/2.png" style="float: left; margin:10px;" /></p>
+
+<p><span style="font-weight: bold;">1 Click Installs</span></p>
+
+<p>50+ 1 click install applications for you to use and experiment with.</p><br />
+
+<p><img width="53" height="53" alt="" src="./images/3.png" style="float: left; margin:10px;" /></p>
+
+<p><span style="font-weight: bold;">Expandable accounts</span></p>
+
+<p><span style="font-weight: bold;"></span>Keep your class projects available to the public from class to class and after you graduate while you are looking for paying clients or jobs.</p><p> Until we get our local servers setup we have found a cool free solution for you to try. Some of our members have used this system and found it to be a nice solution for a small site to test out projects on.</p>                
+                                		<div class="cleared"></div>
+                    </div>
+                </div>
+		<div class="cleared"></div>
+    </div>
+</div>
+<div class="art-box art-block">
+    <div class="art-box-body art-block-body">
+                <div class="art-bar art-blockheader">
+                    <h3 class="t">Need a Free Website?</h3>
+                </div>
+                <div class="art-box art-blockcontent">
+                    <div class="art-box-body art-blockcontent-body">
+                <p style="text-align: center;"><img width="209" height="208" alt="" src="./images/blockimage2.png" /></p>
+
+<p><a href="https://hostzil.la/user/aff.php?aff=345" class="art-button">Start here</a></p>                
+                                		<div class="cleared"></div>
+                    </div>
+                </div>
+		<div class="cleared"></div>
+    </div>
+</div>
+
+                          <div class="cleared"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cleared"></div>
+            <div class="art-footer">
+                <div class="art-footer-body">
+                    
+                            <div class="art-footer-text">
+                                <br />
+Copyleft © 2012 The Next Generation I.T. Club All Rights Public Domain<br />
+<br />
+                                <div class="cleared"></div>
+                                <p class="art-page-footer"></p>
+                            </div>
+                    <div class="cleared"></div>
+                </div>
+            </div>
+    		<div class="cleared"></div>
+        </div>
+    </div>
+    <div class="cleared"></div>
+</div>
+
+</body>
+</html>
